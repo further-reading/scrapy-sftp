4280fbf3d13624c6b20ec978a945facf1f3656a1
further-reading <roy.healy87+gh@gmail.com>
2021-11-12 14:19:40 +0000

    Adding error handling

--- code/sftp_handler.py
+++ code/sftp_handler.py
@@ -11,34 +11,51 @@ class SFTPHandler:
 
     @classmethod
     def from_crawler(cls, crawler):
-        username = crawler.settings.get("SFTP_USER")
-        password = crawler.settings.get("SFTP_PASSWORD")
-        port = crawler.settings.get("SFTP_PORT", 22)
-        host = crawler.settings.get("SFTP_HOST")
-        return cls(username, password, port, host)
+        return cls(crawler)
 
-    def __init__(self, username, password, port, host):
-        self.sftp = self.make_sftp_connection(
-            username,
-            password,
-            port,
-            host,
-        )
+    def __init__(self, crawler):
+        self.username = crawler.settings.get("SFTP_USER")
+        self.password = crawler.settings.get("SFTP_PASSWORD")
+        self.port = crawler.settings.get("SFTP_PORT", 22)
+        self.host = crawler.settings.get("SFTP_HOST")
+        self.tries = crawler.settings.get("SFTP_TRIES", 3)
+        self.sftp = self.make_sftp_connection()
 
     @defers
     def download_request(self, request, spider):
         url = yarl.URL(request.url)
         path = url.path
-        with BytesIO() as stream:
-            self.sftp.getfo(path, stream)
-            stream.seek(0)
-            body = stream.getvalue()
+        try:
+            body = self.sftp_get_data(path)
+            status = 200
+        except FileNotFoundError:
+            body = None
+            status = 404
 
         respcls = responsetypes.from_args(filename=path, body=body)
-        return respcls(url=request.url, body=body)
+        return respcls(url=request.url, body=body, status=status)
 
-    def make_sftp_connection(self, username, password, port, host):
-        t = paramiko.Transport((host, port))
+    def sftp_get_data(self, path):
+        tries = 0
+        while tries < self.tries:
+            with BytesIO() as stream:
+                try:
+                    self.sftp.getfo(path, stream)
+                    stream.seek(0)
+                    return stream.getvalue()
+                except OSError as e:
+                    if "OSError: Socket is closed" in str(e):
+                        # likely timed out, reopen connection
+                        tries += 1
+                        self.sftp = self.make_sftp_connection()
+                    else:
+                        raise e
+
+    def make_sftp_connection(self):
+        t = paramiko.Transport((self.host, self.port))
         t.banner_timeout = 60
-        t.connect(username=username, password=password)
+        t.connect(username=self.username, password=self.password)
         return paramiko.SFTPClient.from_transport(t)
+
+    def close(self):
+        self.sftp.close()
